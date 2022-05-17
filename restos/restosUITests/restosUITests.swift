fccfff0b89b09fb9f971f9d14d7fd8d48865d023
mural <mural86@gmail.com>
2022-05-17 19:27:10 +0200

    uiKit test ui

--- restos/restosUITests/restosUITests.swift
+++ restos/restosUITests/restosUITests.swift
@@ -17,22 +17,14 @@ class restosUITests: XCTestCase {
     override func tearDownWithError() throws {
     }
 
+    //TODO: only for using with UIKit at the moment
     func test_home_screen_enter_detail_and_see_navigation() throws {
         let app = XCUIApplication()
         app.launch()
-
-        let useUIKit = !BundleUtils.getUIModeSwiftUIEnabled()
         
-        if (useUIKit) {
-            let curryGardenCell = app.tables.cells["Curry Garden"]
-            curryGardenCell.tap()
-            XCTAssert(app.staticTexts["Curry Garden"].firstMatch.label.contains("Curry Garden"))
-        } else {
-            let curryGardenRating95Cell = app.tables.cells["Curry Garden, Rating: 9.5"]
-            XCTAssert(curryGardenRating95Cell.label.contains("Curry Garden"))
-            app.tables/*@START_MENU_TOKEN@*/.buttons["Curry Garden, Rating: 9.5"]/*[[".cells[\"Curry Garden, Rating: 9.5\"].buttons[\"Curry Garden, Rating: 9.5\"]",".buttons[\"Curry Garden, Rating: 9.5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-            XCTAssert(app.staticTexts["Curry Garden"].firstMatch.label.contains("Curry Garden"))
-        }
+        let curryGardenCell = app.tables.cells["Curry Garden"]
+        curryGardenCell.tap()
+        XCTAssert(app.staticTexts["Curry Garden"].firstMatch.label.contains("Curry Garden"))        
     }
     
 }
